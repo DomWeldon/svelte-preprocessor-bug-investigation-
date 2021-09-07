04719a1b142db7e1e3682d2c4777a572859192de
Dom Weldon <dom.weldon@decisionlab.co.uk>
2021-09-07 14:50:18 +0100

    working TS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="typescript">
+    let name: string = "world"
+</script>
+
+<h1>Hello {name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
